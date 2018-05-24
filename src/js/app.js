6c545035a39180afa1a7c7187c33f029166f6698
Wojciech Smolarek <kontakt@wojciechsmolarek.pl>
2018-05-24 21:49:37 +0200

    almost working

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,52 @@
-let todoForm = null;
+let todoForm;
 
-document.addEventListener('submit', function() {
-    todoForm = document.querySelector('#content');
+function addTask(text) { 
+    const article = document.createElement('article');
+    article.classList.add('taskContainer');
+
+    const todoHeading = document.createElement('h4');
+    todoHeading.classList.add('taskDate');
+    const date = new Date();
+    const dateText = date.getDate() + '.' + (date.getMonth()+1) + '.' + date.getFullYear() + ', ' + date.getHours() + ':' + date.getMinutes();
+    todoHeading.textContent = dateText;
+
+    const todoI = document.createElement('i');
+    todoI.classList.add('far');
+    todoI.classList.add('fa-times-circle');
+
+    const todoParagraph = document.createElement('p');
+    todoParagraph.classList.add('task');
+    todoParagraph.textContent = text;
+
+    article.appendChild(todoHeading);
+    article.appendChild(todoI);
+    article.appendChild(todoParagraph);
+
+
+    const list = document.querySelector('.list');
+    list.append(article);
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    todoForm = document.querySelector('.content');
+    todoList = document.querySelector('.list');
 
     todoForm.addEventListener('submit', function(e) {
         e.preventDefault();
         const textarea = this.querySelector('textarea');
         if(this.value != '') {
             addTask(textarea.value);
+        }
+        else {
+            alert('error');
+        }
+    })
+
+    todoList.addEventListener('click', function(e) {
+        e.preventDefault();
+        if(e.target.closest('.far') != 0) {
+            e.target.closest('.taskContainer').remove();
         }
     })
 })
