6f54b5c40a8f3e1ac90bb1ded775c8b9a3258247
Wojciech Smolarek <kontakt@wojciechsmolarek.pl>
2018-05-26 13:33:11 +0200

    Working!!

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 let todoForm;
 
-function addTask(text) { 
+
+function addTask(text) {
     const article = document.createElement('article');
     article.classList.add('taskContainer');
 
@@ -10,43 +11,38 @@ function addTask(text) {
     const dateText = date.getDate() + '.' + (date.getMonth()+1) + '.' + date.getFullYear() + ', ' + date.getHours() + ':' + date.getMinutes();
     todoHeading.textContent = dateText;
 
-    const todoI = document.createElement('i');
-    todoI.classList.add('far');
-    todoI.classList.add('fa-times-circle');
+    const todoDeleteBtn = document.createElement('button');
+    todoDeleteBtn.classList.add('.deleteBtn');
+    todoDeleteBtn.innerHTML = '<i class="far fa-times-circle"></i>';
 
     const todoParagraph = document.createElement('p');
     todoParagraph.classList.add('task');
     todoParagraph.textContent = text;
 
     article.appendChild(todoHeading);
-    article.appendChild(todoI);
+    article.appendChild(todoDeleteBtn);
     article.appendChild(todoParagraph);
 
 
     const list = document.querySelector('.list');
     list.append(article);
 
+    todoDeleteBtn.addEventListener('click', function() {
+      document.querySelector('.taskContainer').remove();
+  })
+
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     todoForm = document.querySelector('.content');
-    todoList = document.querySelector('.list');
+    deleteTask = document.querySelector('.deleteBtn');
 
     todoForm.addEventListener('submit', function(e) {
         e.preventDefault();
         const textarea = this.querySelector('textarea');
-        if(this.value != '') {
+        if(textarea.value != '') {
             addTask(textarea.value);
-        }
-        else {
-            alert('error');
-        }
-    })
-
-    todoList.addEventListener('click', function(e) {
-        e.preventDefault();
-        if(e.target.closest('.far') != 0) {
-            e.target.closest('.taskContainer').remove();
         }
     })
 })
